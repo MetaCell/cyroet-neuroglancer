38c45b4e870e2fe994057f6781f3e24098461dd2
Sean Martin <martins7@tcd.ie>
2023-12-01 13:14:34 +0000

    docs: add sharding to annotation example

--- examples/convert_and_view_image_and_annotation.sh
+++ examples/convert_and_view_image_and_annotation.sh
@@ -9,10 +9,10 @@ RELATIVE_RIBO_ANN_PATH="10000_TS_26/Annotations/sara_goetz-ribosome-1.0.json"
 
 cryoet-converter create-image $RELATIVE_TOMOGRAM_PATH -z "$LOCAL_DATA_STORE_PATH/$RELATIVE_TOMOGRAM_PATH" -u $REMOTE_DATA_STORE_URL -r 1.348 -n "TS_026" -o "$LOCAL_DATA_STORE_PATH/TS_026_tomogram.json"
 
-cryoet-converter encode-annotation "$LOCAL_DATA_STORE_PATH/$RELATIVE_RIBO_ANN_PATH" -o "$LOCAL_DATA_STORE_PATH/TS_026_ribosome" -r 1.348 -c 255 0 0 255
+cryoet-converter encode-annotation "$LOCAL_DATA_STORE_PATH/$RELATIVE_RIBO_ANN_PATH" -o "$LOCAL_DATA_STORE_PATH/TS_026_ribosome" -r 1.348 -c 255 0 0 255 --shard-by-id 1
 cryoet-converter create-annotation TS_026_ribosome -z "$LOCAL_DATA_STORE_PATH/$RELATIVE_FA_ANN_PATH" -u $REMOTE_DATA_STORE_URL -n "TS_026_ribosome" -o "$LOCAL_DATA_STORE_PATH/TS_026_ribosome.json" -c "#ff0000 red"
 
-cryoet-converter encode-annotation "$LOCAL_DATA_STORE_PATH/$RELATIVE_FA_ANN_PATH" -o "$LOCAL_DATA_STORE_PATH/TS_026_fatty_acid" -r 1.348 -c 0 0 255 255
+cryoet-converter encode-annotation "$LOCAL_DATA_STORE_PATH/$RELATIVE_FA_ANN_PATH" -o "$LOCAL_DATA_STORE_PATH/TS_026_fatty_acid" -r 1.348 -c 0 0 255 255 --shard-by-id 1
 cryoet-converter create-annotation TS_026_fatty_acid -z "$LOCAL_DATA_STORE_PATH/$RELATIVE_FA_ANN_PATH" -u $REMOTE_DATA_STORE_URL -n "TS_026_fatty_acid" -o "$LOCAL_DATA_STORE_PATH/TS_026_fatty_acid.json" -c "#0000ff blue"
 
 cryoet-converter combine-json "$LOCAL_DATA_STORE_PATH/TS_026_tomogram.json" "$LOCAL_DATA_STORE_PATH/TS_026_ribosome.json" "$LOCAL_DATA_STORE_PATH/TS_026_fatty_acid.json" -o "$LOCAL_DATA_STORE_PATH/TS_026.json" -r 1.348
