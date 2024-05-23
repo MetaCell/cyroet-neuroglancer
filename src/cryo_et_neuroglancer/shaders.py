75e98ce83592fd3b09d7b74c74ca602ee04d243d
Sean Martin <martins7@tcd.ie>
2024-05-23 15:49:29 +0200

    feature: 3d and 2d invertible colormap

--- src/cryo_et_neuroglancer/shaders.py
+++ src/cryo_et_neuroglancer/shaders.py
@@ -6,20 +6,56 @@ def build_shader(shader_parts: list[str]) -> str:
     return "\n".join(shader_parts)
 
 
-def build_invlerp(
+def _build_invlerp(
     name: str, contrast_limits: tuple[float, float], window_limits: tuple[float, float]
 ) -> str:
-    """Builds an invlerp function for a shader."""
-    return f"#uicontrol invlerp {name}(range=[{contrast_limits[0]}, {contrast_limits[1]}], window=[{window_limits[0]}, {window_limits[1]})"
+    return f"#uicontrol invlerp {name}(range=[{contrast_limits[0]}, {contrast_limits[1]}], window=[{window_limits[0]}, {window_limits[1]}], clamp=true)"
+
+
+def _build_invertable_invlerp_getter(name: str):
+    checkbox_part = f"#uicontrol bool invert_{name} checkbox(default=false)"
+    data_value_getter = (
+        f"float {name}_get() {{ return invert_{name} ? 1.0 - {name}() : {name}(); }}"
+    )
+    return checkbox_part, data_value_getter
+
+
+def _build_volume_rendering_switch(if_vr: list[str], if_not_vr: list[str]):
+    indented_if_vr = [f"    {line}" for line in if_vr]
+    indented_if_not_vr = [f"    {line}" for line in if_not_vr]
+    return [
+        "  if (VOLUME_RENDERING) {",
+        *indented_if_vr,
+        "  } else {",
+        *indented_if_not_vr,
+        "  }",
+    ]
 
 
 def get_default_image_shader(
     contrast_limits: tuple[float, float], window_limits: tuple[float, float]
 ):
+    """Generates a default image shader with contrast and window controls."""
+    contrast_name = "contrast"
+    threed_contrast_name = "contrast3D"
     shader_parts = [
-        build_invlerp("contrast", contrast_limits, window_limits),
+        _build_invlerp(contrast_name, contrast_limits, window_limits),
+        _build_invlerp(threed_contrast_name, contrast_limits, window_limits),
+        *_build_invertable_invlerp_getter(contrast_name),
+        *_build_invertable_invlerp_getter(threed_contrast_name),
+        "",
         "void main() {",
-        "  emitGrayscale(contrast());",
+        "  float outputValue;",
+        *_build_volume_rendering_switch(
+            [f"outputValue = {threed_contrast_name}_get();"],
+            [f"outputValue = {contrast_name}_get();"],
+        ),
+        "  emitGrayscale(outputValue);",
+        "  emitIntensity(outputValue);",
         "}",
     ]
     return build_shader(shader_parts)
+
+
+if __name__ == "__main__":
+    print(get_default_image_shader((0.0, 1.0), (0.0, 1.0)))
