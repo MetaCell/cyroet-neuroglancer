532007356852ae72d3d896dc21fbdecb4f983504
Sean Martin <martins7@tcd.ie>
2024-05-30 08:38:19 +0200

    testing: add shader test

---
+++ src/tests/test_shaders.py
@@ -0,0 +1,27 @@
+from cryo_et_neuroglancer.shaders import get_default_image_shader
+
+
+def test_get_default_image_shader():
+    contrast_limits = (0.0, 1.0)
+    window_limits = (0.0, 1.0)
+    expected_shader = """
+#uicontrol invlerp contrast(range=[0.0, 1.0], window=[0.0, 1.0], clamp=true)
+#uicontrol invlerp contrast3D(range=[0.0, 1.0], window=[0.0, 1.0], clamp=true)
+#uicontrol bool invert_contrast checkbox(default=false)
+float contrast_get() { return invert_contrast ? 1.0 - contrast() : contrast(); }
+#uicontrol bool invert_contrast3D checkbox(default=false)
+float contrast3D_get() { return invert_contrast3D ? 1.0 - contrast3D() : contrast3D(); }
+
+void main() {
+  float outputValue;
+  if (VOLUME_RENDERING) {
+    outputValue = contrast3D_get();
+  } else {
+    outputValue = contrast_get();
+  }
+  emitGrayscale(outputValue);
+  emitIntensity(outputValue);
+}
+"""
+    actual_shader = get_default_image_shader(contrast_limits, window_limits)
+    assert actual_shader.strip() == expected_shader.strip()
