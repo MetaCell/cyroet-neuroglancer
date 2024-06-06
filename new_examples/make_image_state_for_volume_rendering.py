c6156a31f68526db4c73c2e85b585fe2ddf5cf60
Sean Martin <martins7@tcd.ie>
2024-06-06 16:09:52 +0200

    feat: also add an img json example

--- new_examples/make_image_state_for_volume_rendering.py
+++ new_examples/make_image_state_for_volume_rendering.py
@@ -2,18 +2,27 @@ import json
 
 from cryoet_data_portal_neuroglancer.state_generator import (
     generate_image_volume_layer,
+    generate_image_layer,
     combine_json_layers,
 )
 
 # Point to some local zarr data - here served by a CORS webserver
 SOURCE = "http://127.0.0.1:9000/00004_sq_df_sorted_zarr/00004_sq_df_sorted"
 
-layer_json = generate_image_volume_layer(
+layer_img_json = generate_image_layer(
     source=SOURCE,
+    scale=(1.0, 1.0, 1.0),
+    size={"x": 1.0, "y": 1.0, "z": 1.0},
     name="Test VR",
 )
+print(layer_img_json)
 
-json_output = combine_json_layers([layer_json], 1.0)
+layer_vr_json = generate_image_volume_layer(
+    source=SOURCE,
+    name="Test Volume Rendering",
+)
+
+json_output = combine_json_layers([layer_vr_json], 1.0)
 
 print(json_output)
 
