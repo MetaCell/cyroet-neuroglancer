9cb5e18623c88268b653b460b3f7ec12a587d7cb
Sean Martin <martins7@tcd.ie>
2024-07-22 14:15:31 +0200

    feat: update vr to new multi-process

--- new_examples/volume_rendering.py
+++ new_examples/volume_rendering.py
@@ -5,6 +5,7 @@ from cryoet_data_portal_neuroglancer.precompute.volume_rendering import volume_r
 from cryoet_data_portal_neuroglancer.precompute.contrast_limits import (
     ContrastLimitCalculator,
 )
+import numpy as np
 
 FILEPATH = r"/media/starfish/LargeSSD/data/cryoET/data/0004_image/Tomograms/VoxelSpacing13.48/CanonicalTomogram/TS_0004.zarr"
 
@@ -15,10 +16,20 @@ contrast_calculator.set_volume_and_z_limits(data_array, central_z_slice=10, z_ra
 contrast_limits = contrast_calculator.contrast_limits_from_percentiles(0.0, 100.0)
 print(contrast_limits)
 
-rendered_image = volume_render(
-    data_array, contrast_limits=contrast_limits, depth_samples=64
+rendered_mem, image_shape = volume_render(
+    data_array, contrast_limits=contrast_limits, depth_samples=256
+)
+volume_rendered_image = np.ndarray(
+    image_shape, dtype=np.float32, buffer=rendered_mem.buf
 )
 
-# Save the RGBA image
-print("Saving the volume rendered image to volume_rendered_image.png")
-plt.imsave("volume_rendered_image.png", rendered_image)
+try:
+    # Save the RGBA image
+    print("Saving the volume rendered image to volume_rendered_image.png")
+    plt.imsave("volume_rendered_image.png", volume_rendered_image)
+    del volume_rendered_image
+except Exception as e:
+    print(f"Error saving the volume rendered image: {e}")
+finally:
+    rendered_mem.close()
+    rendered_mem.unlink()
